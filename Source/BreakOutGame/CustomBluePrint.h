d18f1c21422bab2910b300451cbbec1cdf6f1723
DannyNev-dev <68207283+DannyNev-dev@users.noreply.github.com>
2020-08-06 16:21:32 +1200

    add video and customblueprint

---
+++ Source/BreakOutGame/CustomBluePrint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CustomBluePrint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BREAKOUTGAME_API UCustomBluePrint : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
